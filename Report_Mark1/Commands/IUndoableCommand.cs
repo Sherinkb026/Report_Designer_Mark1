058b4b28d0f4b056e47abbf32789407c21a853b9
Sherinkb026 <sherinkb365@gmail.com>
2025-05-23 10:13:36 -0700

    Ribbon

---
+++ Report_Mark1/Commands/IUndoableCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report_Mark1.Commands
+{
+    public interface IUndoableCommand
+    {
+        void Execute();
+        void Undo();
+    }
+}
